0e3c837ca8d4fcb0557bb7abb04e1d9202118c59
JohnBoyd <dchox111111112334@gmail.com>
2026-02-24 18:21:02 +0000

    Tue, Feb 24, 2026, 10:21 AM -08:00

---
+++ script/loading.js
@@ -0,0 +1,14 @@
+loadText = document.querySelector('.loading-text')
+bg = document.querySelector('.bg')
+
+load = 0
+
+int = setInterval(blurring, 100)
+
+function blurring() {
+    load = load + 1
+
+loadText.innerText = `${load}%`
+loadText.style.opacity = 1-load/100
+bg.style.filter = `blur(${50-load/2}px)`
+}
