ab254a213888b4c2752aec4be9cc6699e0fad19c
JohnBoyd <dchox111111112334@gmail.com>
2026-03-11 16:33:55 +0000

    Wed, Mar 11, 2026, 9:33 AM -07:00

--- script/sound.js
+++ script/sound.js
@@ -1,6 +1,10 @@
 buttons = document.querySelector('.buttons')
+applause = document.querySelector('.applause')
 
 btn = document.createElement('button')
-btn.innerText = "Click Me"
+btn.innerText = "Applause"
 btn.classList.add('btn')
+btn.addEventListener('click', () => {
+    applause.play()
+})
 buttons.appendChild(btn)
