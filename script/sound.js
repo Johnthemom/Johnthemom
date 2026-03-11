49afde8da933b1fcc3653a9de5ac89b7d1dac825
JohnBoyd <dchox111111112334@gmail.com>
2026-03-11 16:46:56 +0000

    Wed, Mar 11, 2026, 9:46 AM -07:00

--- script/sound.js
+++ script/sound.js
@@ -1,13 +1,6 @@
 buttons = document.querySelector('.buttons')
-applause = document.querySelector('.applause')
-boo = document.querySelector('.boo')
-gasp = document.querySelector('.gasp')
-tada = document.querySelector('.tada')
-victory = document.querySelector('.victory')
-wrong = document.querySelector('.wrong')
 
 sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
-
 
 for(let i = 0; i < 6; i = i + 1){
         btn = document.createElement('button')
